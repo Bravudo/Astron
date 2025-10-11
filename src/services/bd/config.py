9fce0172f2ceceefaa744137365f25339fb82abb
Bravudo <gcursino.souza@gmail.com>
2025-10-11 11:55:25 -0300

    add: reconnect railway mode

--- src/services/bd/config.py
+++ src/services/bd/config.py
@@ -8,18 +8,26 @@ save_on = True
 
 load_dotenv()
 api_test = os.getenv("database_url")
-db = mysql.connector.connect(
-        host=os.getenv("database_url").split('@')[1].split(':')[0],
-        port=os.getenv("database_url").split(':')[-1].split('/')[0],
-        user=os.getenv("database_url").split('//')[1].split(':')[0],
-        password=os.getenv("database_url").split(':')[2].split('@')[0],
-        database=os.getenv("database_url").split('/')[-1]
-)
-cursor = db.cursor(dictionary=True)
-print(db)
+
+def check_connection():
+    global db, cursor
+    try:
+        db.ping(reconnect=True, attempts=3, delay=2)
+    except:
+        db = mysql.connector.connect(
+                host=os.getenv("database_url").split('@')[1].split(':')[0],
+                port=os.getenv("database_url").split(':')[-1].split('/')[0],
+                user=os.getenv("database_url").split('//')[1].split(':')[0],
+                password=os.getenv("database_url").split(':')[2].split('@')[0],
+                database=os.getenv("database_url").split('/')[-1]
+        )
+        cursor = db.cursor(dictionary=True)
+        print("Reconectado ao banco")
+
     
 
 async def save_db_new_user(dc_id, join_number, roblox_id, roblox_name, roblox_display):
+        check_connection()
         global save_on
         try:
             cursor.execute(f"SELECT dc_id FROM dc_user WHERE dc_id = %s", (dc_id,))
@@ -38,6 +46,7 @@ async def save_db_new_user(dc_id, join_number, roblox_id, roblox_name, roblox_di
 
 
 async def search_same_data_user(dc_id):
+     check_connection()
      try:
         cursor.execute(f"SELECT join_number FROM dc_user WHERE dc_id = %s",(dc_id,))
         result = cursor.fetchone()
@@ -50,6 +59,7 @@ async def search_same_data_user(dc_id):
 
 
 async def search_last_number():
+    check_connection()
     try:
         cursor.execute("SELECT MAX(join_number) as join_number FROM dc_user")
         result = cursor.fetchone()
@@ -61,6 +71,7 @@ async def search_last_number():
 
 @commands.command()
 async def view_register(ctx):
+    check_connection()
     try:
         cursor.execute("SELECT dc_id, join_number, roblox_id, roblox_name, roblox_display FROM dc_user")
         results = cursor.fetchall()
